8d7b06e4f6559a6573d4de99315130eb139d202b
Sam Marxz <samuel.marques@zenvia.com>
2022-03-29 18:27:04 -0300

    Starter app point

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import * as React from 'react'
+
+import { CharacterType, fetchCharacter } from './characters'
+import { Loading } from './Loading'
+import { CharacterInformation } from './CharacterInformation'
+
 function App() {
-  return <h1>Hello</h1>
+  const [character, setCharacter] = React.useState<CharacterType | null>(null)
+  const [loading, setLoading] = React.useState(true)
+
+  React.useEffect(() => {
+    fetchCharacter().then(c => {
+      setCharacter(c)
+      setLoading(false)
+    })
+  }, [])
+
+  return (
+    <main>
+      {loading && <Loading />}
+      {character && <CharacterInformation character={character} />}
+    </main>
+  )
 }
 
 export default App
---
+++ src/Table.tsx
@@ -0,0 +1,24 @@
+import { CharacterType } from './characters'
+
+type TableProps = { children: React.ReactNode }
+type TableRowProps = {
+  heading: Capitalize<keyof CharacterType>
+  value: CharacterType[keyof CharacterType]
+}
+
+export const Table = ({ children }: TableProps) => {
+  return (
+    <table>
+      <tbody>{children}</tbody>
+    </table>
+  )
+}
+
+export const TableRow = ({ heading, value }: TableRowProps) => {
+  return (
+    <tr>
+      <th>{heading}</th>
+      <td>{value}</td>
+    </tr>
+  )
+}
